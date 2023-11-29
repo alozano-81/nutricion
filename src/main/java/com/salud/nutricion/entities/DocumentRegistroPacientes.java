8f5520f212e2c77854da590a74e7da0b08e57d54
Andres Lozano <yeisonandreslozano@gmail.com>
2023-11-29 15:14:08 -0500

    Cambio nombre de colleccion

--- src/main/java/com/salud/nutricion/entities/DocumentRegistroPacientes.java
+++ src/main/java/com/salud/nutricion/entities/DocumentRegistroPacientes.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
 @Data
-@Document("datosPersonales")
+@Document("pacientes")
 public class DocumentRegistroPacientes {
 
     @Id
