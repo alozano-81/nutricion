4b09170d8e36a2b21454e9fdd3c7623723739746
alozano-81 <yeisonandreslozano@gmail.com>
2025-02-18 18:09:37 -0500

    Ajustes para la creacion de la info de pacientes ya registrados con anterioridad y aplicacion de la mejoras de Java 17 usando record en la encapsulacion de variables.

---
+++ src/main/java/com/salud/nutricion/dto/RegistroInfoPacientesDTO.java
@@ -0,0 +1,7 @@
+package com.salud.nutricion.dto;
+
+//import lombok.Data;
+
+//@Data
+public record RegistroInfoPacientesDTO( Long idPaciente, String motivo, String varios) {   
+} 
---
+++ src/main/java/com/salud/nutricion/entities/DocumentRegistroInfoPacientesDocument.java
@@ -0,0 +1,25 @@
+package com.salud.nutricion.entities;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document("infoPaciente")
+public class DocumentRegistroInfoPacientesDocument {
+
+    @Id
+    private String id;
+    
+
+    private Long idPaciente;
+
+    private String motivo;
+
+    private LocalDateTime fechaNacimiento;
+    
+
+}
